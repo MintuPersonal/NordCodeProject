e5d10de653743426a1ac2b3e6956c6ce1635154e
gmsanzid@gmail.com <gmsanzid@gmail.com>
2020-04-18 18:34:16 +0600

    rrr

--- NordCode/NordCodeClient/src/app/components/shared/footer/footer.component.ts
+++ NordCode/NordCodeClient/src/app/components/shared/footer/footer.component.ts
@@ -14,6 +14,14 @@ export class FooterComponent implements OnInit {
   SettingModel : Ecom_Setting;
   Text : string;
   Mobile: string;
+  Support: string;
+  Facebook: string;
+  GooglePlus: string;
+  Instagram: string;
+  Linked: string;
+  Twtter: string;
+  Youtube: string;
+  Whatapps: string;
   constructor(private sharedService: SharedService) { }
 
   ngOnInit() {    
@@ -21,13 +29,40 @@ export class FooterComponent implements OnInit {
       if (data.status)
         this.Setting = data.setting;
         this.Setting.forEach(item=>{
+         
           if(item.GID == 8){
             this.Text = item.Text 
           } 
           if(item.GID == 8){
             this.Text = item.Text
             this.Mobile = item.GText; 
-          }           
+          }   
+          if(item.GID == 9)  
+          {
+            this.Support = item.Text
+          } 
+          if(item.GID == 2 && item.Code == '1'){
+            this.Facebook = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '2'){
+            this.GooglePlus = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '3'){
+            this.Instagram = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '4'){
+            this.Linked = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '5'){
+            this.Twtter = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '6'){
+            this.Youtube = item.Text;            
+          }
+          if(item.GID == 2 && item.Code == '7'){
+            this.Whatapps = item.Text;            
+          }
+          
         });      
     });
   }
